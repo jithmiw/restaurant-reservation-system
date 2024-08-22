32a20b5dcf2a88a611fb590d4737a6965ab33975
jithmiw <wjithmiravihari@gmail.com>
2024-08-22 23:59:31 +0530

    Created new directories and classes

---
+++ backend/src/main/java/lk/abc/restaurant/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.abc.restaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig {
+}
